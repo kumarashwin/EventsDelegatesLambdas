cc6e1678df2fc89b26b8e1979cacd9db82889e44
Ash Kumar <kumar.ashwin@outlook.com>
2016-07-11 21:19:37 -0400

    Initial commit and done with 'Creating Delegates, Events and EventArgs'

---
+++ EventsDelegatesLambdas/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsDelegatesLambdas
+{  
+
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var worker = new Worker();
+            
+        }
+
+        
+    }
+}
---
+++ EventsDelegatesLambdas/Worker.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsDelegatesLambdas
+{
+    //public delegate int WorkPerformedHandler(object sender, WorkPerformedEventArgs e);
+
+    public class Worker
+    {
+        public event EventHandler<WorkPerformedEventArgs> workPerformed;
+        public event EventHandler workCompleted;
+
+        public Worker()
+        {
+            //workPerformed += (add WorkPerformedEventArgs delegates that encapsulate methods);
+        }
+
+        public void DoWork(int hours, WorkType workType)
+        {
+            for (int i = 0; i < hours; i++)
+            {
+                OnWorkPerformed(i+1, workType);
+                //Raise event for each hour
+            }
+            OnWorkCompleted();
+            //Raise event for work completed
+           
+        }
+
+        protected virtual void OnWorkPerformed(int hours, WorkType workType)
+        {
+            if (workPerformed != null)
+                workPerformed(this,
+                    new WorkPerformedEventArgs(hours, workType));
+        }
+
+        protected virtual void OnWorkCompleted()
+        {
+            if (workCompleted != null)
+                workCompleted(this, EventArgs.Empty);
+        }
+
+    }
+}
